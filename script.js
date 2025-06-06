249a95945c741b0f5040951741172e9790ff4074
ajrogersmd <129768323+ajrogersmd@users.noreply.github.com>
2025-06-06 00:18:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let currentColor = '#FF3B30';
         let currentPDFPage = 1;
         let totalPDFPages = 0;
 
-        const PDF_RENDER_SCALE = 16;
+        const PDF_RENDER_SCALE = 10;
         const pdfControls = document.getElementById('pdfControls');
         const pageIndicator = document.getElementById('pageIndicator');
         const prevPDFBtn = document.getElementById('prevPDFBtn');
